6362ab8ff1c6f227580a54e9e59ba1ecd3d76bad
Артём Лепаев <a_lepaev@scisys.ru>
2024-06-03 12:47:48 +0300

    AddToResultNoAnswer

--- attestationApp/Services/AttestationTasksService.cs
+++ attestationApp/Services/AttestationTasksService.cs
@@ -64,14 +64,28 @@ namespace attestationApp.Services
 
         public async Task SubmitAnswerAsync(int studentId, int questionId, string answerText, bool isCorrect)
         {
-            var answer = new Answer
+            var q = await _context.Questions.Include(q => q.Tests).FirstOrDefaultAsync(x => x.Id == questionId);
+            if (q == null)
             {
-                QuestionId = questionId,
-                Text = answerText,
-                IsCorrect = isCorrect
-            };
+                return;
+            }
+            
+            var test = q.Tests.FirstOrDefault(x => x.QuestionId == questionId);
 
-            _context.Answers.Add(answer);
+
+            var a = _context.Directions.Include(x => x.Students).ToList();
+            var b = a.FirstOrDefault(x => x.Students.Any(y => y.Id == studentId));
+            var c = b?.Id;
+
+            var result = new Result()
+            {
+                StudentId = studentId,
+                TestId = test.Id,
+                MarkId = isCorrect ? 1 : 4,
+                DirectionId =  _context.Directions.Include(x => x.Students).FirstOrDefault(x => x.Students.Any(y => y.Id == studentId))?.Id,
+            };
+            await _context.Results.AddAsync(result);
+
             await _context.SaveChangesAsync();
         }
 
